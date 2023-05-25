a7810f0d15f32dc2b23991907358611fac060191
Md _Shadhin <mdshadhinkn@gmail.com>
2023-05-26 00:26:29 +0600

    create spring boot

---
+++ src/main/java/com/shadhin/thebooks/TheBooksApplication.java
@@ -0,0 +1,13 @@
+package com.shadhin.thebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadhin/thebooks/TheBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadhin.thebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
